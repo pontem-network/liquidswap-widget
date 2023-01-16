62eccb7dce75b9a4a9f5125a7b4a15e07d92101d
Dmitry Kuksenko <lusdjke@gmail.com>
2023-01-16 16:08:16 +0300

    feat(widget): APT-986 fix dialog message

--- src/components/TxSettingsDialog/TxSettingsDialog.vue
+++ src/components/TxSettingsDialog/TxSettingsDialog.vue
@@ -92,17 +92,6 @@ const size = ref<number | undefined>(
 const slippageIsDefault = createSyncRef('isDefault');
 const slippage = createSyncRef('modelValue');
 
-const swapForm = ref();
-
-setTimeout(() => {
-  swapForm.value = document.querySelector('liquid-swap-widget')?.shadowRoot?.querySelector(".swap__header");
-
-  // fix error with scrollable element
-
-  console.log('swapForm.value.parentNode', swapForm.value.parentNode.nodeType);
-
-}, 100);
-
 watch(
     () => slippageIsDefault.value,
     (v) => {
