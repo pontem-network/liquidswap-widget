5b84a037122d196db4d1c77b90693d554c8d8251
Dmitry Kuksenko <lusdjke@gmail.com>
2023-01-24 20:53:55 +0300

    feat(widget): APT-986 add second build config for github pages

---
+++ vite.github.config.js
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vite';
+
+import vue from '@vitejs/plugin-vue';
+
+import { resolve } from 'path';
+
+import svgLoader from 'vite-svg-loader';
+
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.startsWith('liquid-swap')
+        }
+      }
+    }),
+    dts(),
+    svgLoader({
+      defaultImport: 'url'
+    })],
+  resolve: {
+    alias: {
+      '@' : resolve(__dirname, './src')
+    },
+  },
+  optimizeDeps: {
+    include: ['vue', '@vueuse/core'],
+  },
+
+  css: {
+    postcss: 'sass',
+  },
+
+  build: {
+    outDir: 'docs',
+  },
+
+  define: { 'process.env.NODE_ENV': '"production"' },
+
+
+  base: '/liquidswap-widget/',
+  }
+)
