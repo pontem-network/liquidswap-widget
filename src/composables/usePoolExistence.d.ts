a472ee8c50ee86557f740019c81f11c6bf2d048c
Dmitry Kuksenko <lusdjke@gmail.com>
2023-03-24 20:05:30 +0300

    Updates

--- src/composables/usePoolExistence.d.ts
+++ src/composables/usePoolExistence.d.ts
@@ -1,9 +1,11 @@
 import { Ref } from 'vue';
 import { ICreateToken, IPoolExist, TVersionType } from '../types';
+declare type CurveType = 'stable' | 'uncorrelated';
 export declare function usePoolExistence(): {
     isFetching: Ref<boolean>;
     poolExists: Ref<boolean>;
     check: (params: IPoolExist) => Promise<void>;
-    watch: (from: ICreateToken, to: ICreateToken, curve: Ref<string>, version: Ref<TVersionType>) => void;
+    watch: (from: ICreateToken, to: ICreateToken, curve: Ref<CurveType>, version: Ref<TVersionType>) => void;
     reset: () => void;
 };
+export {};
